8d0aa0fa8a6a54c9b482963c0e626986786a8bf6
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-04 23:28:50 -0400

    finished extra extra credit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,45 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a Number");
+        int number = scanner.nextInt();
+        sumOfNumbers(number);
+        gaussianSumOfNumbers(number);
     }
     
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        long startTime = System.nanoTime();
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            answer = answer + i;
+
+
+        }
+        System.out.println("Answer is "+ answer);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        double seconds = (double)totalTime / 1000000000.0;
+        System.out.println("I have fat fingers "+(seconds));
+        return answer;
     }
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        long startTime = System.nanoTime();
+        int answer = (n / 2) * (n + 1);
+        System.out.println("Gaussian answer is "+ answer);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        double seconds = (double)totalTime / 1000000000.0;
+        System.out.println("I have fat fingers "+(seconds));
+        return answer;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
